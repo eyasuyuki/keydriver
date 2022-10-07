e2126f9f5af3a70970d8e9179789b09e4e5b945e
Yasuyuki ENDO <eyasuyuki@gmail.com>
2022-10-07 13:46:01 +0900

    add dummy screenshot.png

--- src/test/java/org/javaopen/keydriver/driver/TestWeb.java
+++ src/test/java/org/javaopen/keydriver/driver/TestWeb.java
@@ -1,12 +1,26 @@
 package org.javaopen.keydriver.driver;
 
+import org.javaopen.keydriver.data.Section;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.io.File;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.withSettings;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 public class TestWeb {
     private Context context;
@@ -15,15 +29,29 @@ public class TestWeb {
 
     @Before
     public void setUp() {
+        Locale.setDefault(Locale.US);
+
         context = new Context();
-        driver = mock(WebDriver.class, withSettings().name("elementName"));//TODO
+        driver = mock(WebDriver.class, withSettings().name("elementName").extraInterfaces(TakesScreenshot.class));//TODO
         element = mock(WebElement.class);
         context.setDriver(driver);
         // TODO when
+        when(((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE))
+            .thenReturn(new File("./src/test/resources/screenshot.png"));
     }
 
     @Test
     public void testOpen() {
+        Map<String, String> map = Stream.of(new String[][] {
+                { "No", "1" },
+                { "Keyword", "open" },
+                { "Target", "url[https://www.example.com]" },
+        }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
+        org.javaopen.keydriver.data.Test test = new org.javaopen.keydriver.data.Test(context, map);
+        Driver d = DriverFactory.getDriver(test);
+        assertThat(d, is(instanceOf(Web.class)));
+        Section section = new Section("Section1");
+        d.perform(context, section, test);
     }
 
     @Test
