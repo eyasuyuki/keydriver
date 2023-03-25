e2f40681aea5e12ab9ae153c335d13ab7cc76191
Yasuyuki ENDO <eyasuyuki@gmail.com>
2023-03-25 12:26:49 +0900

    use DummyTests instead of DummyTest

--- src/test/java/org/javaopen/keydriver/driver/TestDatabase.java
+++ src/test/java/org/javaopen/keydriver/driver/TestDatabase.java
@@ -1,9 +1,9 @@
 package org.javaopen.keydriver.driver;
 
 import org.h2.tools.Server;
-import org.javaopen.keydriver.data.DummyTest;
 import org.javaopen.keydriver.data.Keyword;
 import org.javaopen.keydriver.data.Section;
+import org.javaopen.keydriver.data.DummyTests;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,11 +13,13 @@ import java.sql.SQLException;
 public class TestDatabase {
 
     private Context context;
+    private DummyTests tests;
 
     private Server h2server;
     @Before
     public void setUp() throws SQLException {
         context = Context.getContext(null, "asset/postgresql-42.5.4.jar", "org.postgresql.Driver");
+        tests = new DummyTests(context);
         // run h2 postgresql mode
         String[] options = new String[]{"-pg"};
         h2server = Server.createPgServer(options);
@@ -40,7 +42,7 @@ public class TestDatabase {
 
     @Test
     public void testExecute() {
-        org.javaopen.keydriver.data.Test test = DummyTest.getDummy(Keyword.EXECUTE);
+        org.javaopen.keydriver.data.Test test = tests.getTest(Keyword.EXECUTE);
         Driver d = context.getDriver(test);
         Section section = new Section("Sheet1");
         d.perform(context, section, test);
